e3122089288de4ed4c902d2a0e7ea23b2dfd726b
OllieWilliams <t.giannakodimos@gmail.com>
2020-07-05 17:13:09 -0400

    - Implemented 'addition' and 'memory' routes - Error handling

--- src/main.py
+++ src/main.py
@@ -1,20 +1,69 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
+
 
 app = Flask("Test Server")
+stored_value = 0
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Hello!"
 
 
 @app.route('/addition', methods=['GET', 'POST'])
-def add_variables_with_key():
-    a = float(request.args.get('a'))
-    b = 0
+def add_values_with_key():
+    try:
+        a = request.args.get('a')
+        b = request.args.get('b')
 
-    return str(a + b)
+        if a is None:
+            raise KeyError('Could not find a')
+        if b is None:
+            raise KeyError('Could not find b')
+
+        if not a.isnumeric():
+            raise TypeError('Value of a is not a number')
+        if not b.isnumeric():
+            raise TypeError('Value of b is not a number')
+
+        return jsonify(float(a) + float(b))
+
+    except (TypeError, KeyError) as e:
+        return jsonify('Exception occurred due to invalid input: %s' % str(e)), 400
+    except Exception as e:
+        return jsonify('Error occurred due to server error: %s' % str(e)), 500
+
+
+@app.route('/memory', methods=['PUT'])
+def store_value():
+    try:
+        value_to_store = request.form.get('value')
+
+        if value_to_store is None:
+            raise KeyError('Could not find key: value')
+        if not value_to_store.isnumeric():
+            raise TypeError('value is not a number')
+
+        global stored_value
+        stored_value = value_to_store
+        return jsonify('Value %s has been stored successfully.' % str(stored_value))
+
+    except (KeyError, TypeError) as e:
+        return jsonify('Exception occurred with error: %s' % str(e)), 400
+    except Exception as e:
+        return jsonify('Error occurred due to server error: %s' % str(e)), 500
+
+
+@app.route('/memory', methods=['GET'])
+def retrieve_stored_value():
+    try:
+        global stored_value
+        return jsonify(stored_value)
+
+    except Exception as e:
+        return jsonify('Error occurred due to server error: %s' % str(e)), 500
 
 
 if __name__ == '__main__':
